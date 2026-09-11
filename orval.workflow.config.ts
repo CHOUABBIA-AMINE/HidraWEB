7a4f79a2f423f3a0a5fda0d6e244051c4e62adc1
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-09-11 18:54:18 +0100

    feat(workflow): generate transition execution client

--- orval.workflow.config.ts
+++ orval.workflow.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'orval';
 
-export default defineConfig({ workflow: { input: { target: './openapi/hidra-workflow-af4c3b4723619a25dd9a94f4d27f5a36adab982e.json' }, output: { mode: 'single', target: './src/api/generated/workflow/workflow.ts', schemas: './src/api/generated/workflow/model', client: 'react-query', httpClient: 'axios', clean: true, override: { mutator: { path: './src/api/client/hidraHttpClient.ts', name: 'hidraHttpClient' } } } } });
+export default defineConfig({ workflow: { input: { target: './openapi/hidra-workflow-6e3f3b2829bb63f0d004c8bc9ba386d05eb3edcc.json' }, output: { mode: 'single', target: './src/api/generated/workflow/workflow.ts', schemas: './src/api/generated/workflow/model', client: 'react-query', httpClient: 'axios', clean: true, override: { mutator: { path: './src/api/client/hidraHttpClient.ts', name: 'hidraHttpClient' } } } } });
